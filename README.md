30d919140af52c8a376e5342aad1c550da08cd60
George Nairooze <gnairooze@gmail.com>
2018-10-15 17:26:12 +0200

    add to git

---
+++ README.md
@@ -0,0 +1,11 @@
+# Check Access to IPs
+
+Powershell script to check the access to certain IPs with certain ports.
+
+## check-ip-port.ps1
+
+View the result in powershell IDE or console and export the result to file
+
+## check-ip-port-to-file.ps1
+
+Export the result to file only
---
+++ check-ip-port-to-file.ps1
@@ -0,0 +1,38 @@
+﻿$ResultFile = "d:\test.txt"
+
+$Addresses = @(
+"1.1.1.1:80",
+"1.1.1.2:21"
+)
+
+foreach ($AddressLine in $Addresses) {
+    $AddressLineValues = $AddressLine -split ":"
+    $IP = $AddressLineValues[0]
+    $Port = $AddressLineValues[1]
+    
+    # Create a Net.Sockets.TcpClient object to use for
+    # checking for open TCP ports.
+    $Socket = New-Object Net.Sockets.TcpClient
+        
+    # Suppress error messages
+    $ErrorActionPreference = 'SilentlyContinue'
+        
+    # Try to connect
+    $Socket.Connect($IP, $Port)
+
+    # Make error messages visible again
+    $ErrorActionPreference = 'Continue'
+        
+    # Determine if we are connected.
+    if ($Socket.Connected) {
+        "${IP}`t$Port `topen" | Out-File $ResultFile -Append
+        $Socket.Close()
+    }
+    else {
+        "${IP}`t$Port`tclosed or filtered" | Out-File $ResultFile -Append
+    }
+        
+    # Apparently resetting the variable between iterations is necessary.
+    $Socket.Dispose()
+    $Socket = $null
+}
